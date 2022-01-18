784df116334698f5bbb607ae967555a9b217375b
Michael Harris <xero5k@yahoo.com>
2022-01-18 12:25:18 -0600

    completed assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,29 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def freebie_collection 
+    #returns a collection of freebies from a company
+    self.freebies do |name|
+      name.item_name
+    end 
+  end 
+
+  def dev_collected
+    self.devs do |name|
+      name.name
+    end
+  end 
+
+  def give_freebie(dev, item_name, value)
+    Freebie.create(item_name: item_name, value: value, dev_id: dev.id, company_id: self.id)
+  
+  end 
+
+  def oldset_company
+    f_years = self.companies.each do |name|
+      self.name.founding_year
+    end 
+    f_years.min
+  end 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,32 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  belongs_to :company
+
+  def freebie_collection 
+    #returns a collection of freebies from a company
+    self.freebies do |name|
+      name.item_name
+    end 
+  end 
+
+  def company_collected
+    self.company do |name|
+      name.name
+    end
+  end 
+
+  def received_one?(item_name)
+    self.freebies.each do |i_name|
+      if self.freeies.i_name == item_name
+        true
+      else
+        false
+      end 
+    end
+  end
+
+  def give_away(dev, freebie)
+    freebie.update(dev: dev)
+  end 
 
 end
